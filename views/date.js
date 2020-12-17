e6cee3f1da3b953a954e740565420879ae5ce8e4
Michał Wójcik <michalwojcik@macbook-pro-micha.home>
2020-12-17 15:23:07 +0100

    Initial commit

---
+++ views/date.js
@@ -0,0 +1,12 @@
+﻿exports.getDate = () => {
+
+const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+}
